23e3d208ff682e1fb3e42ace15a0aaa26554ef21
keinn51 <soogood97@gmail.com>
2023-08-05 00:35:45 +0900

    [Feat] block note 붙여보기

---
+++ components/BlockNoteEditor.js
@@ -0,0 +1,28 @@
+'use client'; // this registers <Editor> as a Client Component
+import { BlockNoteEditor } from '@blocknote/core';
+import { BlockNoteView, useBlockNote } from '@blocknote/react';
+import '@blocknote/core/style.css';
+import { useState } from 'react';
+
+// Our <Editor> component we can reuse later
+export default function MyBlockNoteEditor({ ...props }) {
+    const { id, onChange = () => {} } = props;
+    const [markdown, setMarkdown] = useState('');
+    // Creates a new editor instance.
+    const editor = useBlockNote({
+        theme: 'dark',
+        // Listens for when the editor's contents change.
+        onEditorContentChange: (editor) => {
+            // Converts the editor's contents from Block objects to Markdown and
+            // saves them.
+            const saveBlocksAsMarkdown = async () => {
+                const markdown = await editor.blocksToMarkdown(editor.topLevelBlocks);
+                onChange(markdown);
+            };
+            saveBlocksAsMarkdown();
+        },
+    });
+
+    // Renders the editor instance using a React component.
+    return <BlockNoteView id={id} editor={editor} />;
+}
--- pages/til.js
+++ pages/til.js
@@ -1,11 +1,31 @@
-import '@/styles/global/global.scss';
 import styles from '@/styles/til/til.module.scss';
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+
+const MyBlockNoteEditor = dynamic(() => import('@/components/BlockNoteEditor'), { ssr: false });
 
 export default function Til() {
+    const [list, setList] = useState([]);
+    const [contents, setContents] = useState('');
+
     return (
         <div id={styles.til}>
-            <div id={styles.left_container}></div>
-            <div id={styles.right_container}></div>
+            <div id={styles.left_container}>
+                <div id={styles.list}>
+                    {list.map((l, i) => (
+                        <div key={`list-${i}`}>{l}</div>
+                    ))}
+                </div>
+            </div>
+            <div id={styles.right_container}>
+                <MyBlockNoteEditor
+                    id="til_main_text_editor"
+                    onChange={(markdown) => {
+                        setContents(markdown);
+                    }}
+                    onClose={() => {}}
+                />
+            </div>
         </div>
     );
 }
